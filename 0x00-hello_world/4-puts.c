e1734f737b8a750880d679fa3bf7b52fff475094
samtosb <samuelorioha@gmail.com>
2022-12-08 12:37:45 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingua puzzle");
+	    return ( 0);
+}
+
