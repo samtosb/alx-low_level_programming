06fa1d560cbdfa2246e97c025593682038b882d1
samtosb <samuelorioha@gmail.com>
2022-12-08 17:25:20 -0800

    task6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- *  main - Entry prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *  main - Entry point
  * Returen: 1
  */
 int main(void)
 
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
