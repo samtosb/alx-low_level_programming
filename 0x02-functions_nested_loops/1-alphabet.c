28ad8c808206bbab1510a4ff447339400b099578
samtosb <samuelorioha@gmail.com>
2022-12-13 13:24:55 -0800

    _putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Return: Always 0 (success) return the value
+ */
+
+void print_alphabet(void)
+
+{
+	char c= 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+
+}
