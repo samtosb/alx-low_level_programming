d3f1d118c8ae17f640c89c29f94338210cd10cdf
samtosb <samuelorioha@gmail.com>
2022-12-13 14:54:21 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,10 @@
 
 void print_alphabet_x10(void)
 {
-	int ten;
+	int count = 0;
 	char la;
 
-	for (ten = 0; ten < 10; ten++)
+	while (count++ <= 9)
 	{
 		for (la = 'a'; la <= 'z'; la++)
 			_putchar(la);
