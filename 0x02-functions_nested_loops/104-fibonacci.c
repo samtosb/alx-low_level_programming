dbdb772562f1bab1ddafbea4f2a9f5f13c20012c
samtosb <samuelorioha@gmail.com>
2022-12-14 15:54:44 -0800

    task16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,7 +21,7 @@ int main(void)
 
 	for (i = 1; i < 91; i++)
 	{
-		printf(",%lu", aft);
+		printf(", %lu", aft);
 		aft += bef;
 	}
 
@@ -32,8 +32,8 @@ int main(void)
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf("%lu", aft1 + (aft2 / 1));
-		printf("%lu", aft2 % 1);
+		printf("%, lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
