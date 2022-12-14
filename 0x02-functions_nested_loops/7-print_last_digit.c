e14f919a9ea4c74d7f4b42ade00550fffbe15978
samtosb <samuelorioha@gmail.com>
2022-12-14 03:49:57 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,14 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit;
+	int las_digit;
 
 	last_digit = n % 10;
 	if (last_digit < 0)
 	{
 		last_digit = last_digit * -1;
-	{
-	_putchar(last_digit + '0');
-	return (last_digit);
+	}
+	-putcha(last_digit + '0');
+	return (laast_digit);
 
 }
