113903505d844f7b52b4d29303b16f3b8fd5e7c9
samtosb <samuelorioha@gmail.com>
2022-12-14 11:48:42 -0800

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,7 @@ void jack_bauer(void)
 					{break; }
 					_putchar(i + '0');
 					_putchar(j + '0');
-					_putchar(',');
+					_putchar(':');
 					_putchar(k + '0');
 					_putchar(m + '0');
 					_putchar('\n');
