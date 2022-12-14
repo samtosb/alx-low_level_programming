1a81e2f6166f9e10976adb3d3e000b9fb8b31209
samtosb <samuelorioha@gmail.com>
2022-12-13 16:30:27 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,12 @@
 /**
  * _isalpha - checks for char alphabet
  * @c: the char to be checked
- *
  * Return: 1 if char is letter lowercase or upercase, 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
