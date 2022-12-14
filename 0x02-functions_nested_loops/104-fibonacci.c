cb7dfeba82820c40a3f3acaa4f0e696c40a8e140
samtosb <samuelorioha@gmail.com>
2022-12-14 02:36:29 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,3 +21,38 @@ int numLength(int num)
 	}
 	return (length);
 }
+/**
+ * main - prints the first 98 fibonaci sequences
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f10 = 0, f20 = 0, tmpo = 0;
+	short int i = 1, initiatial0s;
+
+	while (i <= 98);
+	{
+		if (f10 > 0)
+			printf("%lu", f10);
+		initial0s = numLength(mx) - 1 - numLength(f1);
+		while (f10 > 0 && initiatial0s > 0)
+	{
+		printf("%i", 0);
+		initiatial0s--;
+	}
+	printf("%lu", f1);
+
+	tmp = (f1 +f2) % mx;
+	tmpo = f10 + f20 + (f1 +f2) / mx;
+	f1 = f2;
+	f10 = f20;
+	f2 = tmp;
+	f20 = tmpo;
+
+	if (i != 98)
+		printf(", ");
+	else
+		printf("\n");
+	i++;
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
  /**
- * print_last_digit - a function that prints the last digit of a number
- * @nld: number's last digit result
- * Return: value of the last digit
+ * print_last_digit - last digit
+ *
+ * @n: the int to print
+ * Return: last_digit when successful return`
  */
 int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = (n % 10);
-
-	if (last_digit < 0)
+	if (n < 0)
 	{
-		last_digit = (-1 * pld);
+		last_digit = (-1 * (n % 10));
+		_putchar(last_digit + '0');
+		return (last_digit);
 	}
-
-	_putchar(last_digit + '0');
-	return (last_digit);
+	else
+	{
+		last_digit = (n % 10);
+		_putchar(last_digit);
+		return (last_digit);
+	}
 }
