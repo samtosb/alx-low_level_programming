eb34f52d56da213594d0d60e135c626480bb56f8
samtosb <samuelorioha@gmail.com>
2022-12-14 09:37:43 -0800

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "6-abs.c"
+
+/**
+ * print_last_digit - last digit
+ *
+ * @n: The int to print
+ * Return: Always 0
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+	-putchar(last_digit + '0');
+	return (last_digit);
+
+}
