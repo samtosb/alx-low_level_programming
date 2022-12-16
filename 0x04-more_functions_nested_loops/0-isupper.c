c1ac95db3aa7164fdda8bddbc93945d5e192fa7a
samtosb <samuelorioha@gmail.com>
2022-12-16 13:10:54 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,17 +7,12 @@
  */
 int _isupper(int c)
 {
-	char uppercase = 'A';
-	int isupper = 0;
-
-	for (uppercase <= 'Z'; uppercase++)
+	if (c >= 65 && c <= 90)
 	{
-		if (c == uppercase)
-		{
-			isupper = 1;
-			break;
-		}
+		return (1);
 	}
-	return (isupper);
-
+	else
+	{
+		return (0);
+	}
 }
