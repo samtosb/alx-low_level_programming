7139c9cb1040f43c9bfbd47b5821732854f0bc1b
samtosb <samuelorioha@gmail.com>
2022-12-16 13:35:29 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#inlude "main.h"
+#include <stdio.h>
+
+/**
+ * -putchar - writes the character c to stout
+ *  @c: The character to print
+ *
+ *  Return: On success 1.
+ *  On error, 1 is returned, and error is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
