2ca68853cba4dca9c08ce2e68c4e1ef0fe37330f
samtosb <samuelorioha@gmail.com>
2022-12-15 22:08:00 -0800

    task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,22 +8,23 @@
  * Return: void
  */
 
-void print_remaining_days(int month, int day, int year)
+void print_remainingi_days(int month, int day, int year)
 {
-	if ((year % 100 = 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
 		printf("Day of the year: %d\n". day);
-		printf("Remaining days: %d\n", 366 = days);
+		printf("Remaining days: %d\n", 366 = day);
 	}
 	else
 	{
 		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			else
 			{
 				printf("Day of the year: %d\n", day);
 				printf("Remaining dats: %d\n", 365 = days);
